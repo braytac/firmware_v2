822b866dbec9abaadbb4b79f4c2969648aa4d38e
Henry <zat_xorg@hotmail.com>
2018-06-18 14:09:14 +0000

    Lectura de tensión - En ASCII con decimales
    Tomar lectura de tensión y enviarla por UART con decimales utilizando la función ftoa, de forma similar al itoa incluída en el ejemplo original.

--- sapi_examples/edu-ciaa-nxp/bare_metal/adc_dac_01/src/adc_dac.c
+++ sapi_examples/edu-ciaa-nxp/bare_metal/adc_dac_01/src/adc_dac.c
@@ -36,7 +36,8 @@
  */
 
 /*==================[inclusions]=============================================*/
-
+#include <stdlib.h>
+#include <math.h>
 //#include "adc_dac.h"   // <= own header (optional)
 #include "sapi.h"        // <= sAPI header
 
@@ -56,35 +57,74 @@
 
 
 /**
- * C++ version 0.4 char* style "itoa":
+* Reemplazado el itoa de Lukás al estar ahora provisto por stdlib
+* C++ version 0.4 char* style "itoa":
  * Written by Lukás Chmela
  * Released under GPLv3.
-
  */
-char* itoa(int value, char* result, int base) {
-   // check that the base if valid
-   if (base < 2 || base > 36) { *result = '\0'; return result; }
 
-   char* ptr = result, *ptr1 = result, tmp_char;
-   int tmp_value;
+// C program for implementation of ftoa()
 
-   do {
-      tmp_value = value;
-      value /= base;
-      *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-   } while ( value );
-
-   // Apply negative sign
-   if (tmp_value < 0) *ptr++ = '-';
-   *ptr-- = '\0';
-   while(ptr1 < ptr) {
-      tmp_char = *ptr;
-      *ptr--= *ptr1;
-      *ptr1++ = tmp_char;
-   }
-   return result;
+// reverses a string 'str' of length 'len'
+void reverse(char *str, int len)
+{
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
 }
 
+ // Converts a given integer x to string str[].  d is the number
+ // of digits required in output. If d is more than the number
+ // of digits in x, then 0s are added at the beginning.
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+
+// Converts a floating point number to string.
+void ftoa(float n, char *res, int afterpoint)
+{
+    // Extract integer part
+    int ipart = (int)n;
+
+    // Extract floating part
+    float fpart = n - (float)ipart;
+
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+
+    // check for display option after point
+    if (afterpoint != 0)
+    {
+        res[i] = '.';  // add dot
+
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter is needed
+        // to handle cases like 233.007
+        fpart = fpart * pow(10, afterpoint);
+
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
 
 /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
 int main(void){
@@ -116,7 +156,8 @@ int main(void){
 
    /* Variable para almacenar el valor leido del ADC CH1 */
    uint16_t muestra = 0;
-
+   float muestra_float = 0;
+   
    /* Variables de delays no bloqueantes */
    delay_t delay1;
    delay_t delay2;
@@ -144,6 +185,15 @@ int main(void){
          uartWriteString( UART_USB, uartBuff );
          uartWriteString( UART_USB, ";\r\n" );
 
+         muestra_float = 330*(float)adcRead( CH1 )/1024;
+         ftoa(muestra_float, uartBuff, 4);
+         uartWriteString( UART_USB, "V en float: " );
+         uartWriteString( UART_USB, uartBuff );
+         uartWriteString( UART_USB, "\r\n" );
+         uartWriteString( UART_USB, "\r\n" );
+
+         
+         
          /* Escribo la muestra en la Salida AnalogicaAO - DAC */
          dacWrite( DAC, muestra );
       }
